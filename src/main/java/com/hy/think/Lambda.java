d54589da71fb5075a8bdc816fa48bb15bc5dbb62
huangyu <1057016567@qq.com>
2017-06-06 23:47:52 +0800

    create

---
+++ src/main/java/com/hy/think/Lambda.java
@@ -0,0 +1,17 @@
+package com.hy.think;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * description:
+ * author: huangyu
+ * create on 17-6-6 下午11:03
+ */
+public class Lambda {
+    public static void main(String[] args){
+        List<String> list = Arrays.asList("world!","hello");
+        list.sort((a,b)->a.compareTo(b));
+        list.forEach(e->System.out.println(e));
+    }
+}
