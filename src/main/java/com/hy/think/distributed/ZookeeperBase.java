007fe2cbe352d64170652e68630a8c2ececcaa93
huangyu0417 <1057016567@qq.com>
2018-07-13 17:02:03 +0800

    zookeeper更新

--- src/main/java/com/hy/think/distributed/ZookeeperBase.java
+++ src/main/java/com/hy/think/distributed/ZookeeperBase.java
@@ -22,16 +22,28 @@ public class ZookeeperBase {
     static final String rootdata = "zookeeper root";
 
     public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
-        ZooKeeper zk = new ZooKeeper("localhost:2181", 10000, event -> {
-            System.out.println(event.getType());
-        });
+        ZooKeeper zk = new ZooKeeper("localhost:2181", 10000, event -> System.out.println(event.getType()));
 
-        String path = zk.create(zkroot, rootdata.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+        Stat s = null;
+        if ((s = zk.exists(zkroot, false)) == null) {
+            System.out.println("not exists node => " + zkroot);
+        } else {
+            System.out.println("exists node => " + zkroot);
+            zk.delete(zkroot, s.getVersion());
+        }
+
+        String path = zk.create(zkroot, rootdata.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         System.out.println("the actual path is " + path);
-        Stat stat = zk.exists(zkroot, true);
+        Stat stat = zk.exists(zkroot, event -> System.out.println("exists watch => " + event.getType()));
         System.out.println(stat.getVersion());
-        zk.delete(path, stat.getVersion());
         zk.close();
+
+        ZooKeeper zk1 = new ZooKeeper("localhost:2181", 10000, event -> System.out.println(event.getType()));
+        if (zk1.exists(zkroot, false) == null) {
+            System.out.println("not exists node => " + zkroot);
+        } else {
+            System.out.println("exists node => " + zkroot);
+        }
     }
 
 
